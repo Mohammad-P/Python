0b67c90d43bf8bc68aff790965d259912fb91f75
Mohammad-P <61448439+Mohammad-P@users.noreply.github.com>
2023-07-16 15:57:46 +0330

    Update SE03_Employees_data.py

--- SE03_Employees_data.py
+++ SE03_Employees_data.py
@@ -1,5 +1,5 @@
 import mysql.connector
-cnx = mysql.connector.connect(user='root', password='Mohammad123', host='127.0.0.1', database='employees')
+cnx = mysql.connector.connect(user='root', password='*********', host='********', database='employees')
 cursor=cnx.cursor()
 query='SELECT * FROM employees ORDER BY Height DESC, Weight ASC;'
 cursor.execute(query)
