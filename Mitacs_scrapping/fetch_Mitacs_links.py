80ec5f65c671251340916a646a9f47d744710265
Mohammad-P <mohammad.pourmand@gmail.com>
2023-07-16 19:11:01 +0330

    Updated

--- Mitacs_scrapping/fetch_Mitacs_links.py
+++ Mitacs_scrapping/fetch_Mitacs_links.py
@@ -5,7 +5,7 @@ import mysql.connector
 site="https://www.mitacs.ca/en/programs/accelerate/open-projects?field_title_value=&field_project_type_value_i18n=All&field_location_s_where_project_c_country=CA&field_location_s_where_project_c_administrative_area=All&field_location_s_where_project_c_locality=&field_desired_education_level_value_i18n%5B0%5D=postdoc&page=1"
 page=requests.get(address)
 soup=bs4(page.content,'html.parser')
-cnx = mysql.connector.connect(user='root', password='Mohammad123', host='127.0.0.1', database='mydb')
+cnx = mysql.connector.connect(user='root', password='*****', host='0.0.0.0', database='mydb')
 link=soup.find_all(onclick=re.compile("location.href"))
 
 count=0
